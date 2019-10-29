3390dedc83dfb58b0e253b509658edb5073d2fd5
jgrindall <john.grindall@gmail.com>
2019-10-29 08:19:44 +0000

    tidy

--- src/babylon/MeshMaker.ts
+++ src/babylon/MeshMaker.ts
@@ -1,11 +1,10 @@
 import * as BABYLON from 'babylonjs';
 
 class MeshMaker {
-  private _scene:BABYLON.Scene;
-  constructor(scene:BABYLON.Scene){
-    this._scene = scene;
-  }
-
+    private _scene:BABYLON.Scene;
+    constructor(scene:BABYLON.Scene){
+      this._scene = scene;
+    }
     make(name:string, props:stringe) : BABYLON.Mesh{
         return BABYLON.MeshBuilder.CreateSphere(name, {segments: 16, diameter: 2}, this._scene);
     }
